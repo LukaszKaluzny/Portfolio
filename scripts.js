6a90940bfc1ad57e26034f176c6094815ab3f6a5
Lukasz Szczesniak <veiner87@gmail.com>
2017-07-22 00:07:24 +0200

    fix scroll in js

--- scripts.js
+++ scripts.js
@@ -1,4 +1,4 @@
-$('.navbar-nav li').on('click', 'a', function(event){
+$('.navbar-nav li, .navbar-header').on('click', 'a', function(event){
   event.preventDefault();
   $('body').animate({
     scrollTop: $($.attr(this, 'href')).offset().top - 50
